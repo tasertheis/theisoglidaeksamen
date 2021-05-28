b5a1da5f0bc906573d0389595885ca761a744413
tasertheis <74410788+tasertheis@users.noreply.github.com>
2021-05-28 05:16:46 +0200

    Delete VatanSporEksamen directory

--- VatanSporEksamen/javascript.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api.jsonbin.io/b/60af6f19893b7c555b1caca6")
-    .then(function (data) {
-        return data.json();
-    })
-    .then(function (post) {
-        const PIN1 = document.getElementById("pin1tekst");
-        PIN1.innerHTML = post.fodbold[0].name + "<br>" + "Adresse: " + post.fodbold[0].address + "<br>" + "Entré: " + post.fodbold[0].entrance + "<br>" + post.fodbold[0].hours
-
-    })
